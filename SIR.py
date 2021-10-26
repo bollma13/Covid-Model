32ff1b44fabe3ff8f1effac8409623a8ab54f3ae
bollma13 <90811136+bollma13@users.noreply.github.com>
2021-10-26 16:10:58 -0400

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -10,7 +10,7 @@ Created on Thu Sep 30 21:06:51 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-#plt.style.use("bmh")
+plt.style.use("bmh")
 from scipy.integrate import solve_ivp
 import dash 
 import dash_html_components as html
@@ -21,7 +21,7 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
 
 
