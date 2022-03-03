8f26861cbf7a488bfd02237c85f93cf82b6b2aff
bollma13 <90811136+bollma13@users.noreply.github.com>
2022-03-03 00:51:53 -0500

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -1024,7 +1024,7 @@ app.layout = html.Div([
                     style_data={
                 'whiteSpace': 'normal',
                 'lineHeight': '15px',
-                
+                'textAlign': 'center'
                     },
                     style_header={'backgroundColor': '#bbbbbb','fontWeight': 'bold'},
                     style_cell={'backgroundColor': '#444444'},
@@ -1111,7 +1111,8 @@ def data_table(N0_value, k1_value, beta_value, ev_value, theta1_value, m_ini_val
     dt = dash.dash_table.DataTable(
                     style_data={
                 'whiteSpace': 'normal',
-                'lineHeight': '15px'
+                'lineHeight': '15px',
+                'textAlign': 'center'
                
                     },
                     style_header={'backgroundColor': '#bbbbbb','fontWeight': 'bold'},
