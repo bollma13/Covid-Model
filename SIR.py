32c4e1a76ec6552a6239909255eed79b659eaf52
bollma13 <90811136+bollma13@users.noreply.github.com>
2022-03-03 00:39:13 -0500

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -1024,7 +1024,7 @@ app.layout = html.Div([
                     style_data={
                 'whiteSpace': 'normal',
                 'lineHeight': '15px',
-                
+                'textAlign': 'center'
                     },
                     style_header={'backgroundColor': '#bbbbbb','fontWeight': 'bold', 'textAlign': 'center'},
                     style_cell={'backgroundColor': '#444444'},
@@ -1112,7 +1112,7 @@ def data_table(N0_value, k1_value, beta_value, ev_value, theta1_value, m_ini_val
                     style_data={
                 'whiteSpace': 'normal',
                 'lineHeight': '15px',
-                 
+                'textAlign': 'center' 
                     },
                     style_header={'backgroundColor': '#bbbbbb','fontWeight': 'bold', 'textAlign': 'center'},
                     style_cell={'backgroundColor': '#444444', 'textAlign': 'center'},
