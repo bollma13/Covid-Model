bbe66d4c547f0accb130841ef3dda3c100d8e695
bollma13 <90811136+bollma13@users.noreply.github.com>
2022-03-03 00:29:17 -0500

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -974,7 +974,7 @@ app.layout = html.Div([
     ]),
     
     ])
-        
+         
     ]),
     
     
@@ -2417,9 +2417,10 @@ def update_graph(k1_value, k2_value,  r_value, gamma1_value, gamma0_value, alpha
         return  dash.no_update, plotly_fig
 
 
-if __name__ == '__main__':
-    app.run_server(debug = True)
+# if __name__ == '__main__':
+#     app.run_server(debug = True)
     
+app.run_server(port=1813,debug=True, host='localhost')
     
     
     
